88cf2191767f250150b67d3593c8ce483b302044
adilraees786 <adilraees911@gmail.com>
2024-05-11 12:30:42 +0500

    random word

---
+++ app.js
@@ -0,0 +1,179 @@
+let alphabat = document.getElementById('head')
+
+
+function a() {
+    let aa = ["Apple", "ant", "anti", "Airoplane", "Air"]
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function b() {
+    let aa = ["Balm", "Blue", "Beseech", "Baffle", "Brace"]
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function c() {
+    let aa = ["cabalas", "cabanas", "cabanes", "cabaret", "cabbage"]
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function d() {
+    let aa = ["Den", "Dirt", "Dolphin", "Dip", "Duck", "Donkey", "Dad", "Dark", "Dance"]
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function e() {
+    let aa = ["Exit", "Exam", "East", "Eagle", "Endow", "Eager", "Edify", "Exact", "Emote"]
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function f() {
+    let aa = ["fall", "friend", "frog", "farm", "fence", "field", "feeling", "fork"]
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+function g() {
+    let aa = ["gamer", "genie", "glass", "grail", "groom", "gruel", "groan", "grant", "grape", "grill", "guil"]
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function h() {
+    let aa = ['Habit', 'Hair', 'Ham', 'Hammer', 'Hand', 'Handle', 'Hanger']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function i() {
+    let aa = ['Ice', 'Inlet', 'Invent', 'Iron', 'Icecream', 'Injury', 'Idea', 'Iceberg']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function j() {
+    let aa = ["john", "sat", "james", "jark", "january"]
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+
+function k() {
+    let aa = ['kindness', 'knowledgeable', 'kudos', 'kids', 'karma', 'kiss', 'kinship']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function l() {
+    let aa = ['List', 'Late', 'Lamp', 'Lion', 'Liquid', 'Listening', 'Language', 'Lips', 'Lady']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+
+function m() {
+    let aa = ['Money', 'Melt', 'My', 'Mat', 'Monkey', 'Man', 'Mix', 'Mud']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function n() {
+    let aa = ['Nag', 'Net', 'Normal', 'Nail', 'New', 'Nose', 'Name', 'Newborn']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function o() {
+    let aa = ['Oats', 'Ointment', 'Or', 'Of', 'Once', 'Oven', 'Off', 'One', 'Over', 'Office', 'Onion']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function p() {
+    let aa = ['Pan', 'Pant', 'Paint', 'Pet', 'Petrol', 'Pick', 'Pig', 'Pigeon', 'Pin']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function q() {
+    let aa = ['quite', 'Quietly', 'Quail', 'queue', 'Quarter']
+
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function r() {
+    let aa = ['Rabbit', 'Raccoon', 'Race', 'Racket', 'Radar', 'Radical', 'Radio', 'Radish']
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function s() {
+    let aa = ['smoke', 'setup', 'sixty', 'saint', 'salty', 'sadly', 'saute', 'salad', 'saggy']
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function t() {
+    let aa = ['Table', 'Treat', 'Touch', 'Tooth', 'Throw', 'Teach', 'Tempo', 'Taboo']
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function u() {
+    let aa = ['Utopia', 'Umpteen', 'Unction', 'Unrequited', 'Ubiquitous', 'Unbeknownst']
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function v() {
+    let aa = ['Very', 'Vote', 'Vibe', 'Vent', 'Vault', 'Visitor', 'Vacate', 'Volume.']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+
+function w() {
+    let aa = ['Way', 'Wall', 'Word', 'Wolf', 'Waves', 'Water', 'Wing', 'Week']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+
+function x() {
+    let aa = ['xyst', 'xanthic', 'xenial', 'xenon', 'xebecs', 'xylophone', 'Xavier', 'X-ray fish']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+function y() {
+    let aa = ['Yes', 'You', 'Year', 'Yak', 'Yelp', 'Yawn', 'Yield', 'Yearly']
+
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+}
+
+
+
+function z() {
+    let aa = ['Zip', 'Zag', 'Zest', 'Zoom', 'Zone', 'Zillion', 'Zigzag', 'Zenith']
+    let ran = Math.floor(Math.random() * aa.length);
+    alphabat.innerText = aa[ran]
+} 
